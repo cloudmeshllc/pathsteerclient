8f116cebd9b9e1ab9183aa0114d732c583357b65
cloudmeshllc <mike.sanchez@cloudmeshllc.com>
2026-02-24 16:00:01 +0000

    Auto backup 20260224_160001

--- scripts/controller-route-switch.sh
+++ scripts/controller-route-switch.sh
@@ -13,7 +13,7 @@ SSH_OPTS="-o BatchMode=yes -o ConnectTimeout=3 -o StrictHostKeyChecking=no"
 declare -A SVC_DEV=([fa]="svc_fa" [fb]="svc_fb" [sl_a]="svc_sl_a" [sl_b]="svc_sl_b" [cell_a]="svc_ca" [cell_b]="svc_cb")
 declare -A SVC_GW=([fa]="10.203.1.2" [fb]="10.203.2.2" [sl_a]="10.203.3.2" [sl_b]="10.203.4.2" [cell_a]="10.203.5.2" [cell_b]="10.203.6.2")
 declare -A OUTER_DEV=([fa]="outer_fa" [fb]="outer_fb" [sl_a]="outer_sl_a" [sl_b]="outer_sl_b" [cell_a]="outer_ca" [cell_b]="outer_cb")
-declare -A OUTER_GW6=([fa]="fd10:fa::2" [fb]="fd10:fb::2" [sl_a]="fd10:sl_a::2" [sl_b]="fd10:sl_b::2" [cell_a]="fd10:ca::2" [cell_b]="fd10:cb::2")
+declare -A OUTER_GW6=([fa]="fd10:fa::2" [fb]="fd10:fb::2" [sl_a]="fd10:a1::2" [sl_b]="fd10:b1::2" [cell_a]="fd10:ca::2" [cell_b]="fd10:cb::2")
 
 DEV="${SVC_DEV[$UPLINK]}"
 GW="${SVC_GW[$UPLINK]}"
